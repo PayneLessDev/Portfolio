cc0a4e805115e4a5131b84a0382f4fb6903e1460
Adam Payne <adam@paynlessdev.tech>
2021-09-01 20:37:46 +1000

    finally 100vh

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Select the button
+const btn = document.querySelector(".btn-toggle");
+ 
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle("dark-theme");
+});
