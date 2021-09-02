00188fa09ffb9826c69fb27cc182d4205dad154f
Adam Payne <adam@paynlessdev.tech>
2021-09-03 02:09:22 +1000

    hamberger toggler toggles

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
-const checkbox = document.querySelector("input[id=tgl]");
+const checkbox = document.getElementById("tgl");
+const burger = document.getElementById("burger")
+const overlay = document.getElementById("overlay")
+
+
+
 checkbox.addEventListener('change', function(){
-  document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("dark-theme");
+});
+
+burger.addEventListener('change', function(){
+    document.getElementById("overlay").classList.toggle("hidden");
 });
