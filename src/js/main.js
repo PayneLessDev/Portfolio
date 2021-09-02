02083d9563ad7af338a4e1ca307a964eaacb6ad1
Adam Payne <adam@paynlessdev.tech>
2021-09-02 14:17:03 +1000

    pure css

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,4 @@
-// Select the button
-// const btn = document.querySelector(".btn-toggle");
 const checkbox = document.querySelector("input[id=tgl]");
- 
-// Listen for a click on the button
-// btn.addEventListener("click", function() {
 checkbox.addEventListener('change', function(){
-  // Then toggle (add/remove) the .dark-theme class to the body
   document.body.classList.toggle("dark-theme");
 });
